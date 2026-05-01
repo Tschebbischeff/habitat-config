9f527df56df6fe0ceab3e6cf886f0f019c758ba6
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-05-01 22:57:40 +0200

    builds image for amd64 and arm64

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 repositoryNamespace=""
-pushAfterBuild=""
+pushAfterBuild="_"
 while [ "$#" -gt "0" ]; do
     [ "$1" == "--" ] && shift && break
-    [ "$1" == "--push" ] && pushAfterBuild="_"
+    [ "$1" == "--no-push" ] && pushAfterBuild=""
     # Positional arguments
     if [[ ! ( "$1" =~ ^-.* ) ]]; then
         { [ -z "$repositoryNamespace" ] && { repositoryNamespace="$1"; true; } } ||
@@ -13,15 +13,29 @@ while [ "$#" -gt "0" ]; do
     shift
 done
 
+[ -n "$pushAfterBuild" ] && [ -z "$repositoryNamespace" ] && { echo "When pushing a repository name must be supplied."; exit 1; }
+
 currentVersion="$(cat ./metadata.json | jq -r '.version')"
 currentVersionMaj="$(echo "$currentVersion" | grep -Po '^(0|[1-9][0-9]*)')"
 currentVersionMajMin="$(echo "$currentVersion" | grep -Po '^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)')"
 currentVersionMajMinPat="$(echo "$currentVersion" | grep -Po '^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)')"
+
+docker buildx create \
+    --use \
+    --name habitat-config-builder \
+    node-amd64
+docker buildx create \
+    --append \
+    --name habitat-config-builder \
+    node-arm64
 
 docker build \
     -t "$([ -n "$repositoryNamespace" ] && echo "$repositoryNamespace/")habitat-config:$currentVersionMajMinPat" \
     -t "$([ -n "$repositoryNamespace" ] && echo "$repositoryNamespace/")habitat-config:$currentVersionMajMin" \
     -t "$([ -n "$repositoryNamespace" ] && echo "$repositoryNamespace/")habitat-config:$currentVersionMaj" \
     -t "$([ -n "$repositoryNamespace" ] && echo "$repositoryNamespace/")habitat-config:latest" \
+    --platform linux/amd64,linux/arm64 \
     "$([ -n "$pushAfterBuild" ] && echo "--push")" \
     ./habitat-config
+
+docker buildx stop habitat-config-builder
