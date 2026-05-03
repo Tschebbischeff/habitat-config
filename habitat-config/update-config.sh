5a2390d67ff35177d451a13135978dd4189e9e58
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-05-03 17:33:38 +0200

    adds orchestration of consuming and providing configurations, leaves final consumer stage to be defined by the module consuming the configuration

--- habitat-config/update-config.sh
+++ habitat-config/update-config.sh
@@ -1,28 +1,107 @@
 #!/usr/bin/env bash
 
+# ### Init
+
 cd "/habitat-config" || exit 1
 
-for dirPath in "./source/"*; do
-    moduleName="$(basename "$dirPath")"
-    echo "*** Applying configuration for module '$moduleName' (Source directory: /habitat-config/source/$moduleName)..."
-    # Traefik Dynamic Config
-    if [ -d "./target/traefik" ]; then
-        echo "Traefik config:"
-        for f in "./target/traefik/"*; do
-            filename=$(basename -- "$f")
-            filename="${filename%.*}"
-            [[ "$filename" == *".$moduleName" ]] && rm -f "$f"
-        done
-        if [ -d "$dirPath/traefik" ]; then
-            for f in "$dirPath/traefik/"*; do
-                filename=$(basename -- "$f")
-                extension="${filename##*.}"
-                filename="${filename%.*}"
-                echo " $f -> ./target/traefik/$filename.$moduleName.$extension"
-                cp -f "$f" "./target/traefik/$filename.$moduleName.$extension";
-            done
-        else
-            echo " None"
-        fi
-    fi
+[ -z "${APP_SESSION_ID}" ] && { echo "No APP_SESSION_ID provided, cannot synchronize configuration."; exit 1; }
+
+ORCH_PATH="./orchestration"
+ORCH_SESSION_PATH="$ORCH_PATH/${APP_SESSION_ID}"
+SRC_PATH="./source"
+TGT_PATH="./target"
+declare -a CONSUMERS
+[ -d "$SRC_PATH/.consume" ] && for filePath in "$SRC_PATH/.consume/"*; do
+    fileName="$(basename "$filePath")"
+    fileExtension="${fileName##*.}"
+    [ "$fileExtension" != "sh" ] && continue
+    consumerName="${fileName%.*}"
+    CONSUMERS["${#CONSUMERS[@]}"]="$consumerName"
 done
+
+
+# ### Init Orchestration
+
+for dirPath in "$ORCH_PATH"*; do
+    [ ! "$(basename "$dirPath")" == "${APP_SESSION_ID}" ] && rm -rf "$dirPath" 2>/dev/null
+done
+[ ! -d "$ORCH_SESSION_PATH" ] || { echo "This session has already been configured. Skipping everything and exiting with assumed success."; exit 0; }
+mkdir -p "$ORCH_SESSION_PATH"
+
+for consumerName in "${CONSUMERS[@]}"; do
+    # shellcheck disable=SC2115 # ORCH_SESSION_PATH cannot be empty
+    [ -d "$ORCH_SESSION_PATH/$consumerName" ] && rm -rf "$ORCH_SESSION_PATH/$consumerName"
+    mkdir -p "$ORCH_SESSION_PATH/$consumerName"
+done
+touch "$ORCH_SESSION_PATH/.${APP_MODULE_NAME}.started"
+echo "Waiting for all modules to start..."
+while :; do
+    sleep 1
+    for moduleName in "${APP_MODULES[@]}"; do
+        [ ! -f "$ORCH_SESSION_PATH/.$moduleName.started" ] && continue
+    done
+    break
+done
+echo "All modules started."
+
+
+# ### Provide Own Configurations
+
+echo "*** Providing configuration..."
+
+anyProviders=""
+for serviceSrcPath in "$SRC_PATH/"*; do
+    anyProviders="_"
+    consumerName="$(basename "$serviceSrcPath")"
+    if [ ! -d "$ORCH_SESSION_PATH/$consumerName" ]; then
+        echo "No consumers for configuration target '$consumerName', skipping."
+        continue
+    fi
+    echo "Copying configuration for target '$consumerName'..."
+    cp -rp "$serviceSrcPath" "$ORCH_SESSION_PATH/$consumerName/${APP_MODULE_NAME}"
+    # # Traefik
+    # if [ -d "$SRC_PATH/traefik" ]; then
+    #     echo "Applying traefik config:"
+    #     for f in "./source/traefik/dynamic/"*; do
+    #         filename=$(basename -- "$f")
+    #         extension="${filename##*.}"
+    #         filename="${filename%.*}"
+    #         echo " $f -> ./target/traefik/$filename.$moduleName.$extension"
+    #         cp -f "$f" "./target/traefik/$filename.$moduleName.$extension";
+    #     done
+    #     if inArray "traefik" "${#WAIT_ON_TARGETS[@]}"; then
+    #     fi
+    # else
+    #     echo "No traefik config to apply from this module."
+    # fi
+done
+[ -z "$anyProviders" ] && echo "This module does not provide any configuration."
+touch "$ORCH_SESSION_PATH/.${APP_MODULE_NAME}.finished"
+
+# ### Merge Configurations on Consumer Side
+
+echo "*** Consuming configuration..."
+
+finalExitCode="0"
+if [ "${#CONSUMERS[@]}" -gt "0" ]; then
+    echo "Waiting for all modules to finish their providing stage..."
+    for moduleName in "${APP_MODULES[@]}"; do
+        [ ! -f "$ORCH_SESSION_PATH/.$moduleName.finished" ] && continue
+    done
+    for consumerName in "${CONSUMERS[@]}"; do
+        if [ -z "$(ls -A "$ORCH_SESSION_PATH/$consumerName")" ]; then
+            echo "No modules have provided configuration for target '$consumerName', skipping execution of consumer."
+        fi
+        echo "Starting consumer for target '$consumerName'..."
+        if ! "$SRC_PATH/.consume/$consumerName.sh" "$ORCH_SESSION_PATH/$consumerName" "$TGT_PATH/$consumerName"; then
+            echo "Consumer for target '$consumerName' failed with exit code: $?"
+            finalExitCode="2"
+        fi
+    done
+else
+    echo "This module does not consume any configuration."
+fi
+
+# ### Exit
+
+exit "$finalExitCode"
